700791ac5eef44ead1351e3436b825a763eb79ae
Cuong Nguyen <68762617+imaqtbike@users.noreply.github.com>
2021-11-09 20:25:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 function getCountryData() {
-    const request = fetch("https://restcountries.com/v3.1/name/peru")
+    const request = fetch("https://restcountries.com/v3.1/name/")
 request.then((res) => {
    return res.json()
 }).then(data => console.log(data))
